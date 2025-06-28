bb41f7a1210d7cb3b38785cdbfc6036b7ca2c406
Boubacar Sy <76449404+CarbabouSy@users.noreply.github.com>
2025-06-28 10:12:02 +0000

    Update welcomeRoutes.js

--- dist/routes/welcomeRoutes.js
+++ dist/routes/welcomeRoutes.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.get("/", (req, res) => {
-    res.send("Welcome to my app");
+    res.send("Welcome to my app Baka Tira");
 });
 exports.default = router;
